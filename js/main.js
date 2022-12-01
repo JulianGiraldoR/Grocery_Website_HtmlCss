0a4e82e80d44427083e7b22531e35160ff182b4c
JulianGiraldoR <juliangato87@gmail.com>
2022-12-01 02:09:34 -0500

    diseño responsive y menu desplegable con javascript

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle');
+/*===============Show Menu===========================*/
+if(navToggle) {
+    navToggle.addEventListener('click',() => {
+        navMenu.classList.toggle('show-menu');
+    });
+}
